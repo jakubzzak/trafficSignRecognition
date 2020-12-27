5a079f188f5bd6086cb1bc6db3ab1067f621c69b
jakubzzak <zak.jakub67@gmail.com>
2020-12-27 18:03:41 +0100

    Initial Commit

---
+++ traficSignRecognition/traficSignRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  traficSignRecognitionApp.swift
+//  traficSignRecognition
+//
+//  Created by Jakub Žák on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct traficSignRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
